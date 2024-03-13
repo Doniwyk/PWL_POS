e2a9aff47a2356ca16a5713607876b63c3e7bc39
doniwyk <doniwyk@gmail.com>
2024-03-13 09:30:06 +0700

    update UserController and UserModel with fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,21 +10,13 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-        // $data = [
-        //     'username' => 'customer 1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data);
-
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-
-        $data = ['nama' => 'Pelanggan Pertama'];
-        UserModel::where('username', 'customer 1')->update($data);
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
