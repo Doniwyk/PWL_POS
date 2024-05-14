370e1997027a6d0ba1ba3a25807b261b8502a9d0
doniwyk <doniwyk@gmail.com>
2024-05-15 04:00:06 +0700

    JS10 - practicum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,22 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Model implements AuthenticatableContract
-{
-    use HasFactory, Authenticatable;
+class UserModel extends Authenticatable implements JWTSubject{
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims(){
+        return[];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    public function level(): BelongsTo
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id');
+    }
+    public function stok(): HasMany{
+        return $this->hasMany(StokModel::class, 'user_id', 'user_id');
+    }
+    public function transaksi(): HasMany{
+        return $this->hasMany(TransaksiModel::class, 'user_id', 'user_id');
     }
 }
